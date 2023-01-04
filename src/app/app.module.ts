0deb769427f24f0fc53fcad23a1cff63b7f2274f
Gabriel Morales <gabrielm@proactinfo.com>
2023-01-03 21:45:31 -0500

    depencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
+// provides
+import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
+import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
+
+//
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    BarcodeScanner,
+    NativeStorage,
+    InAppBrowser,
+    SQLite,
+    SQLiteObject,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/services/data-local.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataLocalService {
+  private dbInstance?: SQLiteObject;
+
+  readonly db_name: string = "productos.db";
+  readonly db_table: string = "productos";
+  constructor(private platform: Platform,  private sqlite: SQLite,) {
+    this.databaseConn();
+  }
+
+  databaseConn() {
+    this.platform.ready().then(() => {
+      this.sqlite.create({
+          name: this.db_name,
+          location: 'default'
+        }).then((sqLite: SQLiteObject) => {
+          this.dbInstance = sqLite;
+          sqLite.executeSql(`
+              CREATE TABLE IF NOT EXISTS ${this.db_table} (
+                _id  INTEGER PRIMARY KEY
+                nombre TEXT,
+                tipo TEXT,
+                marca TEXT,
+                precio REAL,
+                proveedor TEXT,
+              )`, [])
+            .then((res) => {
+              alert(JSON.stringify(res));
+            })
+            .catch((error) => alert(JSON.stringify(error)));
+        })
+        .catch((error) => alert(JSON.stringify(error)));
+    });   
+}
+}
